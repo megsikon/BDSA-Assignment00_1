1bd4d2a2f8e1e4fa92c0f43a6d73cad8eadfb527
Magnus Emil Gervig Svensson <mesv@itu.dk>
2022-09-02 14:42:46 +0200

    created ui for isLeapYear

--- LeapYear.Tests/UnitTest1.cs
+++ LeapYear.Tests/UnitTest1.cs
@@ -53,4 +53,60 @@ public class LeapYearTest
       //assert
       Assert.True(!res);
     }
+
+    [Fact]
+    public void _2004isLeapYearStr()
+    {
+      //arrange
+      int leapYear = 2004;
+
+      //act
+      var isLeap = Program.isLeapYear(leapYear);
+      var isLeapStr = Program.isLeapYearStr(isLeap);
+
+      //assert
+      Assert.Equal("yay", isLeapStr);
+    }
+
+    [Fact]
+    public void _2005isNotLeapYearStr()
+    {
+      //arrange
+      int leapYear = 2005;
+
+      //act
+      var isLeap = Program.isLeapYear(leapYear);
+      var isLeapStr = Program.isLeapYearStr(isLeap);
+
+      //assert
+      Assert.Equal("nay", isLeapStr);
+    }
+
+    [Fact]
+    public void _1600isLeapYearStr()
+    {
+      //arrange
+      int leapYear = 1600;
+
+      //act
+      var isLeap = Program.isLeapYear(leapYear);
+      var isLeapStr = Program.isLeapYearStr(isLeap);
+
+      //assert
+      Assert.Equal("yay", isLeapStr);
+    }
+
+    [Fact]
+    public void _1700isNotLeapYearStr()
+    {
+      //arrange
+      int leapYear = 1700;
+
+      //act
+      var isLeap = Program.isLeapYear(leapYear);
+      var isLeapStr = Program.isLeapYearStr(isLeap);
+
+      //assert
+      Assert.Equal("nay", isLeapStr);
+    }
 }
--- LeapYear/Program.cs
+++ LeapYear/Program.cs
@@ -4,8 +4,19 @@ public class Program
 {
     //int leapYear;
     static void Main(string[] args) {
-        Console.WriteLine(isLeapYear(2004));
-    } 
+        Console.Write("Enter possible leap year: ");
+        string yearStr = Console.ReadLine();
+        int year = Convert.ToInt32(yearStr);
+        Console.WriteLine(isLeapYearStr(isLeapYear(year)));
+    }
+
+    public static string isLeapYearStr(bool isLeap) {
+      if (isLeap) {
+        return "yay";
+      } else {
+        return "nay";
+      }
+    }
     public static bool isLeapYear(int year)
     {
         if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))) {
